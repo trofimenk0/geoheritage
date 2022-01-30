9780d5e2fc0483ec9c524de8ed57af86199bf20a
trofimenk0 <trofimenkodpwork@gmail.com>
2022-01-30 17:37:21 +0200

    Livesearch styles

--- app/wp-content/themes/GeoHeritage/inc/ajax.php
+++ app/wp-content/themes/GeoHeritage/inc/ajax.php
@@ -48,6 +48,10 @@ function geo_search_geological_monuments() {
         while ( $the_query->have_posts() ): $the_query->the_post(); ?>
 
 			<a href="<?php echo esc_url( post_permalink() ); ?>" class="header__searchResultLink">
+				<svg class="header__searchResultIcon" width="20" height="10" viewBox="0 0 20 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M1.9 5C1.9 3.29 3.29 1.9 5 1.9H8.05C8.57467 1.9 9 1.47467 9 0.95V0.95C9 0.425329 8.57467 0 8.05 0H5C2.24 0 0 2.24 0 5C0 7.76 2.24 10 5 10H8.05C8.57467 10 9 9.57467 9 9.05V9.05C9 8.52533 8.57467 8.1 8.05 8.1H5C3.29 8.1 1.9 6.71 1.9 5ZM6 5C6 5.55228 6.44772 6 7 6H13C13.5523 6 14 5.55228 14 5V5C14 4.44772 13.5523 4 13 4H7C6.44772 4 6 4.44772 6 5V5ZM15 0H11.95C11.4253 0 11 0.425329 11 0.95V0.95C11 1.47467 11.4253 1.9 11.95 1.9H15C16.71 1.9 18.1 3.29 18.1 5C18.1 6.71 16.71 8.1 15 8.1H11.95C11.4253 8.1 11 8.52533 11 9.05V9.05C11 9.57467 11.4253 10 11.95 10H15C17.76 10 20 7.76 20 5C20 2.24 17.76 0 15 0Z" fill="#52d858"/>
+				</svg>
+
 				<?php the_title(); ?>
 			</a>
 
